b8da4502a96a3079c4c2905e2197c87790c138e9
euzebio13k <euzebio13k@gmail.com>
2026-09-09 11:28:19 -0300

    projeto final

--- app/DataBase.php
+++ app/DataBase.php
@@ -1,13 +1,12 @@
 <?php
 namespace App;
-use App\Fornecedor;
 use PDO;
 use PDOException;
 class DataBase{
     private const HOST = 'localhost';
     private const USER = 'root';
     private const PASSWORD = '123';
-    private const DBNAME = 'supermercado';
+    private const DB = 'supermercado';
     private $connection;
     private $table;
     //metodo que constroi a classe
@@ -17,14 +16,18 @@ class DataBase{
     }
     //metodo que cria uma conexão com o banco de dados
     public function setConnection(){
-        $connection = new PDO('mysql:host='.self::HOST.';dbname='.self::DBNAME,self::USER,self::PASSWORD);
+        $this->connection = new PDO('mysql:host='.self::HOST.';dbname='.self::DB,self::USER,self::PASSWORD);   
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     //metodo que insere dados no banco
-    public function execute($query, $values){
+    public function execute($query, $values = null){
         try{
             echo "<pre>";
             print_r($query);
             echo "</pre>";
+            $statement = $this->connection->prepare($query);
+            $statement->execute($values);
+            return $statement;
         }catch(PDOException $e){
             die('ERRO: '.$e);
         }
@@ -35,18 +38,38 @@ class DataBase{
         //criar um array com valores = ?
         $binds = array_pad([], count($array),'?');
         //monta a query
-        $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).')
-        VALUES('.implode(',',$binds).')';
+        $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
         //executa a query
         $this->execute($query, array_values($array));
         return $this->connection->lastInsertId();
     }
+    public function update($where, $array){
+        //extrair as chaves do array
+        $fields = array_keys($array);
+        //monta a query
+        $query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).' WHERE '.$where;
+        //executa a query
+        $this->execute($query, array_values($array));
+        return true;  
+    }
+    public function delete($where){
+        //monta a query
+        $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
+        $this->execute($query);
+        return true;
+    }
+    public function select($where=null,$order=null,$limit=null,$fields='*'){
+        $where = strlen($where) ? 'ORDER BY '.$where : '';
+        $order = strlen($order) ? 'ORDER BY '.$order : '';
+        $limit = strlen($limit) ? 'LIMIT '.$limit : '';
+        $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order.' '.$limit;
+        return $this->execute($query);        
+    }
+    
 }
 $db = new DataBase('fornecedor');
-$db->insert([
-    'nome'=>'Coca-cola',
-    'cnpj'=>'6745765678687',
-    'telefone'=>'756786',
-    'email'=>'coca@gmail.com',
-    'endereco'=>'Avenida JK'
-]);
+$db->delete('id=1');
+$f = $db->select(null,'cnpj desc',2,'nome, cnpj')->fetchAll(PDO::FETCH_CLASS);
+echo "<pre>";
+print_r($f);
+echo "</pre>";
